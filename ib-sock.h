4d57f4ae99f0874c8b3ed8086b1d146ccf95dfdb
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-14 11:49:20 +0300

    connect ib socket to the destination. It's not easy as traditional socket API, we need to create resourses ourself and wait until address + route will resolved.

--- ib-sock.h
+++ ib-sock.h
@@ -11,9 +11,18 @@
 #include <linux/socket.h>
 #include <linux/in.h>
 
+#include <linux/poll.h>
+
 struct IB_SOCK;
 
 struct IB_SOCK *ib_socket_create(void);
 void ib_socket_destroy(struct IB_SOCK *sock);
+
+/* async. return POLLOUT or POLLERR events */
+int ib_socket_connect(struct IB_SOCK *sock, struct sockaddr_in  *dstaddr);
+void ib_socket_disconnect(struct IB_SOCK *sock);
+
+/* sleep to wait poll event from socket */
+unsigned long ib_socket_poll(struct IB_SOCK *sock);
 
 #endif
