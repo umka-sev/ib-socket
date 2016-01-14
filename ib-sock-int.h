43659a9cfa4c891d86627a67790d1127b16f8d08
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-14 11:49:20 +0300

    connect ib socket to the destination. It's not easy as traditional socket API, we need to create resourses ourself and wait until address + route will resolved.

--- ib-sock-int.h
+++ ib-sock-int.h
@@ -7,9 +7,32 @@
 #include <rdma/ib_verbs.h>
 #include <rdma/rdma_cm.h>
 
+#define IB_CQ_EVENTS_MAX 128
+
+#define IB_ADDR_TIMEOUT 100
+#define IB_ROUTE_TIMEOUT 100
+
 struct IB_SOCK {
 	/* primary OFED stack ID */
 	struct rdma_cm_id	*is_id;
+
+	/* transfer related parts */
+	/* completion events */
+	struct ib_cq		*is_cq;
+	/* queue pair to communicate between nodes */
+	struct ib_qp		*is_qp;
+
+	/* event mask */
+	unsigned long		is_events;
 };
+
+/* messages on wire */
+#define WIRE_ATTR	__attribute__((packed))
+
+#define IB_HELLO_MAGIC 0x9012
+
+struct ib_hello {
+	__u32	magic;
+} WIRE_ATTR;
 
 #endif
