a8779477f53eae8f093d741234c6c58aa3f6f7b0
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-14 11:49:20 +0300

    connect ib socket to the destination. It's not easy as traditional socket API, we need to create resourses ourself and wait until address + route will resolved.

--- ib-sock-int.h
+++ ib-sock-int.h
@@ -7,9 +7,30 @@
 #include <rdma/ib_verbs.h>
 #include <rdma/rdma_cm.h>
 
+#define IB_CQ_EVENTS_MAX 128
+
+#define IB_ADDR_TIMEOUT 100
+#define IB_ROUTE_TIMEOUT 100
+
 struct IB_SOCK {
 	/* primary OFED stack ID */
 	struct rdma_cm_id	*is_id;
+
+	/* transfer related parts */
+	/* completion events */
+	struct ib_cq		*is_cq;
+	/* queue pair to communicate between nodes */
+	struct ib_qp		*is_qp;
+
+	/* event mask */
+	unsigned long		is_events;
 };
+
+/* messages on wire */
+#define WIRE_ATTR	__attribute__((packed))
+
+struct ib_hello {
+	__u32	magic;
+} WIRE_ATTR;
 
 #endif
--- ib-sock.c
+++ ib-sock.c
@@ -1,6 +1,30 @@
 #include "ib-sock.h"
 #include "ib-sock-int.h"
 
+static int
+cm_client_handler(struct rdma_cm_id *cmid, struct rdma_cm_event *event)
+{
+	struct IB_SOCK *sock = cmid->context;
+	int ret = 0;
+
+	switch (event->event) {
+	case RDMA_CM_EVENT_ADDR_RESOLVED:
+		ret = rdma_resolve_route(cmid, IB_ROUTE_TIMEOUT);
+		break;
+	case RDMA_CM_EVENT_ROUTE_RESOLVED:
+		break;
+	case RDMA_CM_EVENT_ESTABLISHED:
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	printk("client CM event ret %d\n", ret);
+	return ret;
+}
+
+
 static int cm_handler(struct rdma_cm_id *cmid, struct rdma_cm_event *event)
 {
 	int ret = 0;
@@ -14,6 +38,7 @@ static int cm_handler(struct rdma_cm_id *cmid, struct rdma_cm_event *event)
 	case RDMA_CM_EVENT_ROUTE_RESOLVED:
 	case RDMA_CM_EVENT_ESTABLISHED:
 	case RDMA_CM_EVENT_CONNECT_RESPONSE:
+		ret = cm_client_handler(cmid, event);
 		break;
 	/* server related events */
 	case RDMA_CM_EVENT_CONNECT_REQUEST:
@@ -66,4 +91,46 @@ void ib_socket_destroy(struct IB_SOCK *sock)
 	if (sock->is_id)
 		rdma_destroy_id(sock->is_id);
 	kfree(sock);
+}
+
+/***************************************************************************************/
+/* 
+ * In IB terms - connect is resolving address + router + some connect mgs exchange.
+ * all of it done via CM related events.
+ *
+ * IB uses an IP addresses as some ID to resolve in destination GID value.
+ */
+
+int ib_socket_connect(struct IB_SOCK *sock, struct sockaddr_in  *dstaddr)
+{
+	struct sockaddr_in  srcaddr;
+	int err;
+
+        memset(&srcaddr, 0, sizeof(srcaddr));
+        srcaddr.sin_family      = AF_INET;
+
+	/* IB needs to start from resolve addr / route first */
+	err = rdma_resolve_addr(sock->is_id, (struct sockaddr *)&srcaddr, 
+			        (struct sockaddr *)dstaddr, 
+			        IB_ADDR_TIMEOUT /* timeout ms */
+			        );
+	printk("resolve dst address status %d\n", err);
+
+	/* when connect will done - WRITE event will generated */
+	return err;
+}
+
+void ib_socket_disconnect(struct IB_SOCK *sock)
+{
+	int err = 0;
+
+	/* change the ib conn state only if the conn is UP, however always call
+	 * rdma_disconnect since this is the only way to cause the CM to change
+	 * the QP state to ERROR
+	 */
+
+	err = rdma_disconnect(sock->is_id);
+	if (err)
+		printk("Failed to disconnect, conn: 0x%p err %d\n",
+			 sock,err);
 }
