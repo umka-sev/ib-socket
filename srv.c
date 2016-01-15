6443cdd392ead7befc35b39d1c1686a1a9db3f02
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-15 12:45:24 +0300

    Introduce a server side code for a socket.  a) create a server socket  b) accept an incomming connect request  c) hello exchange

--- srv.c
+++ srv.c
@@ -1,14 +1,26 @@
 #include "ib-sock.h"
 
+
+
 static int __init
 srv_init(void)
 {
 	struct IB_SOCK *sock;
+	unsigned long event;
+	int ret;
 
 	sock = ib_socket_create();
 	if (sock == NULL)
 		return -ENOMEM;
 
+	ret = ib_socket_bind(sock, INADDR_ANY, 1998);
+	if (ret < 0)
+		goto out;
+
+	event = ib_socket_poll(sock);
+	/* wait a POLLIN to say new connection ready to */
+
+out:
 	ib_socket_destroy(sock);
 	return 0;
 }
