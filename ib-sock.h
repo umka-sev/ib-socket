68314e2bec210f85d5bb9fbe98ee45de84413583
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-21 10:12:50 +0300

    attach control message to IB
    use IB verbs api to attach control messages
    to incomming ring.
    Send ctl mgs on wire to verify recv code works

--- ib-sock.h
+++ ib-sock.h
@@ -28,7 +28,18 @@ unsigned long ib_socket_poll(struct IB_SOCK *sock);
 int ib_socket_bind(struct IB_SOCK *sock, uint32_t addr, unsigned port);
 struct IB_SOCK *ib_socket_accept(struct IB_SOCK *parent);
 
+/* return a size of transfer */
+size_t ib_socket_read_size(struct IB_SOCK *sock);
+/* submit to read.
+ * read protocol
+ * first POLLIN event have result to call ib_socket_read_try to get a buffer size
+ *  and call of ib_socket_read will send request to read and attach a buffers
+ * but return a EAGAIN to wait a transfer done
+ * next POLLIN event will inducate a transfer done  */
+int ib_socket_read(struct IB_SOCK *sock, void *buf, size_t size);
+
+
 /* send some amount data over wire */
-size_t ib_socket_write(struct IB_SOCK *sock, void *buf, size_t size);
+int ib_socket_write(struct IB_SOCK *sock, void *buf, size_t size);
 
 #endif
