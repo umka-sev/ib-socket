378b1691a8ab43f6c31822718042c6569f2afc00
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-01-21 10:12:50 +0300

    attach control message to IB
    use IB verbs api to attach control messages
    to incomming ring.
    Send ctl mgs on wire to verify recv code works

--- ib-sock-ctl.c
+++ ib-sock-ctl.c
@@ -42,7 +42,26 @@ ib_sock_ctl_take(struct IB_SOCK *sock)
  */
 int ib_sock_ctl_post(struct IB_SOCK *sock, struct ib_sock_ctl *msg)
 {
-	return 0;
+	/* base on ib_mad_post_receive_mads() - that wr isn't need to live any time 
+	 * so - save memory and declare on stack.
+	 */
+	struct ib_recv_wr wr;
+	struct ib_recv_wr *bad_wr;
+	struct ib_device *device = sock->is_id->device;
+
+	/* trasnfer ownership to the device */
+	ib_dma_sync_single_for_device(device, 
+				   msg->iscm_sge.addr, msg->iscm_sge.length,
+				   DMA_FROM_DEVICE);
+
+	msg->iscm_flags |= CTL_MSG_RX;
+
+	wr.next = NULL;
+	wr.wr_id = (uintptr_t)msg;
+	wr.sg_list = &msg->iscm_sge;
+	wr.num_sge = 1;
+
+	return ib_post_recv(sock->is_qp, &wr, &bad_wr);
 }
  
 void ib_sock_ctl_put(struct IB_SOCK *sock, struct ib_sock_ctl *msg)
@@ -54,15 +73,45 @@ void ib_sock_ctl_put(struct IB_SOCK *sock, struct ib_sock_ctl *msg)
 	wake_up(&sock->is_ctl_waitq);
 }
 
+static int 
+ctl_msg_init(struct IB_SOCK *sock, struct ib_sock_ctl *msg)
+{
+	struct ib_device *device = sock->is_id->device;
+	unsigned long dma_addr;
+
+	dma_addr = ib_dma_map_single(device, (void *)&msg->iscm_msg,
+				sizeof(msg->iscm_msg), DMA_FROM_DEVICE);
+	if (ib_dma_mapping_error(device, dma_addr))
+		return -EIO;
+
+	msg->iscm_flags = 0;
+	msg->iscm_sge.addr = dma_addr;
+	msg->iscm_sge.length = sizeof(msg->iscm_msg);
+	msg->iscm_sge.lkey   = sock->is_mem.ism_mr->lkey;
+
+	return 0;
+}
+
+static void
+ctl_msg_fini(struct IB_SOCK *sock, struct ib_sock_ctl *msg)
+{
+	struct ib_device *device = sock->is_id->device;
+
+	ib_dma_unmap_single(device, msg->iscm_sge.addr,
+			    sizeof(msg->iscm_msg), DMA_FROM_DEVICE);
+}
+
 int ib_sock_ctl_init(struct IB_SOCK *sock)
 {
 	struct ib_sock_ctl *msg;
 	unsigned int i;
 	unsigned count = 0;
+	int ret;
 
 	init_waitqueue_head(&sock->is_ctl_waitq);
 	INIT_LIST_HEAD(&sock->is_ctl_active_list);
 	INIT_LIST_HEAD(&sock->is_ctl_idle_list);
+	INIT_LIST_HEAD(&sock->is_ctl_rd_list);
 
 	/* preallocate until limit.
 	 * we can allocate it by request, but simplify a code
@@ -71,15 +120,23 @@ int ib_sock_ctl_init(struct IB_SOCK *sock)
 		msg = kmalloc(sizeof(*msg), GFP_KERNEL);
 		if (msg == NULL)
 			continue;
-		count ++;
 		/* pre init */
+		if (ctl_msg_init(sock, msg) != 0) {
+			kfree(msg);
+			continue;
+		}
+		count ++;
 		ib_sock_ctl_put(sock, msg);
 	}
 	/* half of mgs uses for incomming, half outgoning */
 	count /= 2;
 	for(i = 0; i < count; i++) { 
 		msg = ib_sock_ctl_take(sock);
-		ib_sock_ctl_post(sock, msg);
+		ret = ib_sock_ctl_post(sock, msg);
+		if (ret != 0) {
+			msg->iscm_flags  = 0;
+			ib_sock_ctl_put(sock, msg);
+		}
 	}
 
 	return 0;
@@ -89,15 +146,17 @@ void ib_sock_ctl_fini(struct IB_SOCK *sock)
 {
 	struct ib_sock_ctl *pos, *next;
 
-	/* XXX flush active */
+	/* if QP destroyed it is safe to unmap memory and free */
 	list_for_each_entry_safe(pos, next, &sock->is_ctl_active_list, iscm_link) {
 		list_del(&pos->iscm_link);
+		/* active transfer should be aborted before */
 		list_add(&pos->iscm_link, &sock->is_ctl_idle_list);
 	}
 
 	list_for_each_entry_safe(pos, next, &sock->is_ctl_idle_list, iscm_link) {
 		list_del(&pos->iscm_link);
 
+		ctl_msg_fini(sock, pos);
 		kfree(pos);
 	}
 }
--- srv.c
+++ srv.c
@@ -7,6 +7,8 @@ srv_init(void)
 {
 	struct IB_SOCK *sock, *sock_child = NULL;
 	unsigned long event = 0;
+	bool read_st;
+	void *data;
 	int ret;
 
 	sock = ib_socket_create();
@@ -28,13 +30,28 @@ srv_init(void)
 	/* have incomming event, so socket is ready */
 	sock_child = ib_socket_accept(sock);
 	BUG_ON(sock_child == NULL);
-#if 0
+
 	while ( 1 ) {
 		event = ib_socket_poll(sock_child);
 		if (event & POLLERR)
 			break;
+		if (!read_st && (event & POLLIN)) {
+			size_t d_size;
+
+			d_size = ib_socket_read_size(sock);
+			data = kmalloc(d_size, GFP_KERNEL);
+			if (data == NULL)
+				break;
+			read_st = true;
+		}
+		if (read_st && (event & POLLIN)) {
+			/* read done */
+			printk("%s\n", data);
+			kfree(data);
+			read_st = false;
+		}
 	}
-#endif
+
 	ib_socket_destroy(sock_child);
 out:
 	ib_socket_destroy(sock);
