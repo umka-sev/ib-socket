5b96e5a7f37b6fe35548c50b90ac5f62cde2d573
Alexey Lyashkov <shadow@Alexeys-MacBook-Pro.local>
2016-02-02 16:47:27 +0300

    Refresh of ctl-verbs.patch

--- cli.c
+++ cli.c
@@ -36,16 +36,16 @@ cli_init(void)
 		printk("error connect \n");
 		goto exit;
 	}
-	event = ib_socket_poll(sock);
-	printk("Event hit %lx\n", event);
-	if (event & POLLOUT) {
-		err = ib_socket_write(sock, srv_addr, strlen(srv_addr));
-		printk("status send %d\n", err);
+	while ( 1 ) {
+		event = ib_socket_poll(sock);
+		printk("Event hit %lx\n", event);
+		if (event & POLLERR)
+			break;
+		if (event & POLLOUT) {
+			err = ib_socket_write(sock, srv_addr, strlen(srv_addr));
+			printk("sends status %d\n", err);
+		}
 	}
-	/* next event when transfer is done */
-	printk("Event hit %lx\n", event);
-	event = ib_socket_poll(sock);
-
 
 	ib_socket_disconnect(sock);
 exit:
